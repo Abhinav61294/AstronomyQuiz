104d279821beb7ec17bbfc19a029d824241593ab
Tip_Time <abhinav61294@gmail.com>
2023-05-16 22:53:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,74 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String[] question ={
+            "1. What is the dark center of a sunspot called?",
+            "2. A year on this planet is shorter than a day.",
+            "3. Which planets do not have a natural satellite?",
+            "4. When was Pluto removed from the list of planets?",
+            "5. The amount of light emitted by an object in a unit of time is known as:",
+            "6. Who discovered the laws of planetary motion?",
+            "7. In which year was India''s first mission to Mars Mangalyaan launched?",
+            "8. Which planet''s axis is almost parallel to its orbital plane, making it spin nearly on its side?",
+            "9. With which US spacecraft did Neil Armstrong and Edwin Aldrin, Jr. land on the moon?",
+            "10. The number of terrestrial planets in the solar system is:",
+            "11. What is the only planet not named after a God?",
+            "12. Who had proved first that our earth and another planet are revolving?",
+            "13. Which one of the following planet is also called morning star or evening star?",
+            "14. The device employed to measure the diameters of stars and our galaxy (Milky Way) is called:",
+            "15. The position of a planet when it is nearest to the Sun is called",
+            "16. Which one of the following planets takes the same number of days for rotation and revolution?",
+            "17. One Astronomical Unit is the average distance between",
+            "18. What is the approximate mean velocity with which the Earth moves round the Sun in its orbit?",
+            "19. The planet whose density is less than water and on keeping in the water it will start to float:",
+            "20. Phobos and Deimos are the natural satellites of which planet?"
+    };
+
+    public static String[][] choices = {
+            {"Filament","Penumbra","Umbra","Dark Spot"},
+            {"Venus","Mars","Uranus","Neptune"},
+            {"Mercury and Venus","Venus and Mars","Mercury and Uranus","Venus and Neptune"},
+            {"2009","2006","2000","2003"},
+            {"Thermal Radiation","Luminosity","Starlight","Black body radiation"},
+            {"Newton","Galileo","Kepler","Copernicus"},
+            {"2009","2006","2000","2013"},
+            {"Venus","Mars","Uranus","Neptune"},
+            {"Apollo-12","Apollo-11","Apollo-7","Apollo-17"},
+            {"4","7","3","1"},
+            {"Jupiter","Earth","Uranus","Neptune"},
+            {"Aristotle","Galileo","Copernicus","Edwin Hubble"},
+            {"Venus","Mars","Uranus","Neptune"},
+            {"Photometer","Barometer","Viscometer","Interferometer"},
+            {"Apogee","Aphelion","Perigee","Perihelion"},
+            {"Jupiter","Earth","Uranus","Mercury"},
+            {"Earth and Sun","Venus and Earth","Mercury and Sun","Sun and Neptune"},
+            {"20km/s","30km/s","40km/s","50km/s"},
+            {"Jupiter","Saturn","Uranus","Mercury"},
+            {"Jupiter","Mars","Pluto","Mercury"}
+    };
+
+    public static String[] correctAnswers = {
+            "Umbra",
+            "Venus",
+            "Mercury and Venus",
+            "2006",
+            "Luminosity",
+            "Kepler",
+            "2013",
+            "Uranus",
+            "Apollo-11",
+            "4",
+            "Earth",
+            "Copernicus",
+            "Venus",
+            "Interferometer",
+            "Perihelion",
+            "Mercury",
+            "Earth and Sun",
+            "30km/s",
+            "Saturn",
+            "Mars"
+    };
+
+}
